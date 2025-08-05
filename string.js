bc9b274df9c6cba679006d7dad655da12f45982d
Sourav Sarker <souravsarker036@gmail.com>
2025-08-06 03:58:07 +0600

    string array length

---
+++ string.js
@@ -0,0 +1,19 @@
+// string is immutable ---> not changeable
+// array is immutable ---> changeable
+const country = 'Bangladesh';
+const division = "rangpur";
+const district = `Bangladesh`;
+// const thana = new String('Demra');  /*object*/
+const numbers =[1, 2, 3, 4, 5]
+console.log(numbers)
+
+numbers[2]= 20;
+console.log(numbers) /*change the value of index*/
+
+// console.log(numbers.length)
+// console.log(numbers[2])
+
+// console.log(typeof thana)
+
+// console.log(country.length)
+// console.log(country[1]) find index element
