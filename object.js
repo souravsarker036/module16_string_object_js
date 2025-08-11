9b391360d83b9d858b38a1218a4a9ffac0e3a3aa
Sourav Sarker <souravsarker036@gmail.com>
2025-08-11 22:09:53 +0600

    object

---
+++ object.js
@@ -0,0 +1,23 @@
+const age = 21;
+const school = 'abcd'
+const isPassed = true;
+// let isDeveloper;
+
+
+//data type: non premitive
+const subjects = ['bangla','english','physics','math']
+const bottle = {
+    brand: 'apple', 
+    price: 45,
+    color: 'white',
+    isClean: false,
+}
+const subject = {
+    name: 'Biology',
+    teacher: 'Rahim sir',
+    chapter: ['first','second'],
+    exam: {
+        name: 'final exam',
+        marks: 100,
+    }
+}
